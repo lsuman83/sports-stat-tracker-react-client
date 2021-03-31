c33bcc32f7eb4d677bb545447d8a8e46b11e09ce
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-03-31 00:05:43 -0400

    wip

--- sports-stat-tracker-react-client/src/containers/TeamShowContainer.js
+++ sports-stat-tracker-react-client/src/containers/TeamShowContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PlayerList from '../components/PlayerList.js'
 import GamesList from '../components/GamesList.js'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { fetchTeam } from '../actions/teams'
 
@@ -11,32 +12,18 @@ class TeamShowContainer extends Component {
     state = {
         team: {},
         players: [],
+        games: [],
         loading: true
     }
 
     componentDidMount(){
-        return fetch(`http://localhost:3001/teams/${this.props.match.params.teamID}`, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(resp => resp.json())
-            .then(({team, players}) => {
-                    setTimeout(() => {
-                        this.setState({
-                            team,
-                            players,
-                            loading: false
-                        })
-                    }, 1000)
-                }) 
+        const teamId = this.props.match.params.teamID
+        this.props.getTeam(teamId)
     }
     
     render() {
 
-        if(this.state.loading){
+        if(this.props.loadingState !== 'successful'){
             return(
                 <button type='button' className='rounded-lg ml-10 mt-5 bg-blue-800 text-center ...' disabled>
                     <svg className='animate-pulse h-5 w-5 mr-3 color-black ...' viewBox='0 0 24 24'>
@@ -45,18 +32,23 @@ class TeamShowContainer extends Component {
                 </button>
             )
         }
+        
         return(
             <section>
-                <div className='grid auto-rows-min'>
-                    <div className='text-center'>
-                        <h1 className='font-mono text-xl text-center mt-10'><strong>{this.props.team.name}</strong></h1>
+                <div>
+                    <h1 className='font-mono text-xl text-center mt-10'><strong>{this.props.team.name}</strong></h1>
+                    <div className='grid grid-rows-3 justify-center'>
                         <div>{this.props.team.location}</div>
                         <div>{this.props.team.sport}</div>
                         <div>{this.props.team.league_name}</div>
                     </div>
                     <PlayerList key={this.props.team.id} players={this.props.players} />
-                    
                 </div>
+                <button type='submit' className='rounded-md bg-blue-500 p-1 ml-20'>
+                    <Link to={`/teams/${this.props.match.params.teamID}/players/new`}>
+                        New Player
+                    </Link>
+                </button>
             </section>
         )
     }
@@ -65,9 +57,9 @@ class TeamShowContainer extends Component {
 
 const mapStateToProps = (state, { match }) => {
     const teamId = match.params.teamID
-    const loadingState = state.players.teamsLoaded[teamId] || 
+    const loadingState = state.players.teamsLoaded[teamId] && 
     state.games.teamsLoaded[teamId] || 'notStarted'
-
+    
     return {
         team: state.teams.list.find(team => team.id == teamId),
         players: state.players.list.filter(player => player.team_id == teamId),
