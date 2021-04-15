0a9cde95a17c0aefd2374dca24497127ec0d8107
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-04-15 17:11:19 -0400

    created reducers for auth

--- sports-stat-tracker-react-client/src/reducers/index.js
+++ sports-stat-tracker-react-client/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux'
 import teamsReducer from './teams.js'
 import playersReducer from './players.js'
-
+import authReducer from './auth.js'
 
 export default combineReducers({
     teams: teamsReducer,
-    players: playersReducer
+    players: playersReducer,
+    auth: authReducer
 })
